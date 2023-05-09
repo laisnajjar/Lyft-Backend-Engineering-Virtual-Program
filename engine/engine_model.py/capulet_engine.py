d353871b3cd552c744e9a9b7f774d8b98b9f853f
laisnajjar <laisnajj@umich.edu>
2023-05-09 12:55:24 -0400

    first commit

---
+++ engine/engine_model.py/capulet_engine.py
@@ -0,0 +1,13 @@
+from abc import ABC
+
+from engine.engine_model import Engine
+
+
+class CapuletEngine(Engine):
+    def __init__(self, current_mileage, last_service_mileage):
+        self.current_mileage = current_mileage
+        self.last_service_mileage = last_service_mileage
+        self.capulet_milage_need_service = 30000
+
+    def needs_service(self):
+        return self.current_mileage - self.last_service_mileage > self.capulet_milage_need_service
