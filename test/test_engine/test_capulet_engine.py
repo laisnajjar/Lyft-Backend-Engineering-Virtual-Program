59b3d25a6df2983e98d0c15c137936c1a7d3e21f
laisnajjar <laisnajj@umich.edu>
2023-05-11 16:34:15 -0400

    unit test cases

---
+++ test/test_engine/test_capulet_engine.py
@@ -0,0 +1,19 @@
+import unittest
+from engine.engine_model.capulet_engine import CapuletEngine
+
+class TestCapuletEngine(unittest.TestCase):
+    def test_engine_should_be_serviced(self):
+        last_service_date = datetime.today().date()
+        current_mileage = 30001
+        last_service_mileage = 0
+
+        engine = CapuletEngine(current_mileage, last_service_mileage)
+        self.assertTrue(engine.needs_service())
+
+    def test_engine_should_not_be_serviced(self):
+        last_service_date = datetime.today().date()
+        current_mileage = 30000
+        last_service_mileage = 0
+
+        engine = CapuletEngine(current_mileage, last_service_mileage)
+        self.assertFalse(engine.needs_service())
---
+++ test/test_engine/test_willoughby_engine.py
@@ -0,0 +1,19 @@
+import unittest
+from engine.engine_model.willoughby_engine import WilloughbyEngine
+
+class TestWilloughbyEngine(unittest.TestCase):
+    def test_engine_should_be_serviced(self):
+        last_service_date = datetime.today().date()
+        current_mileage = 60001
+        last_service_mileage = 0
+
+        engine = WilloughbyEngine(current_mileage, last_service_mileage)
+        self.assertTrue(engine.needs_service())
+
+    def test_engine_should_not_be_serviced(self):
+        last_service_date = datetime.today().date()
+        current_mileage = 60000
+        last_service_mileage = 0
+
+        engine = WilloughbyEngine(current_mileage, last_service_mileage)
+        self.assertFalse(engine.needs_service())
